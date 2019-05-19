6faa19ecb528d099b9e9dea0996e08914da69c76
Andrés  Felipe Mora Castro <llaandresmcll@gmail.com>
2019-05-18 23:14:34 -0500

    se cambia la configuracio de la bd agregando mlab mongdb

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost/db-dejavu';
+const url = 'mongodb://luna:luna2017@ds145916.mlab.com:45916/db-dejavu';
 
 const connect = (async () => await mongoose.connect(url, { useNewUrlParser: true })
     .then(() => console.log('se ha conectado a la bd'))
